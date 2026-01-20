7252bdaa1b83931129470090f1b998b1256105c7
Edson Moretti <edsonmoretti@live.com>
2026-01-20 10:46:14 -0300

    Add initial springboot project setup with Maven configuration and Spring Boot application

---
+++ backend/src/main/java/br/com/edsonmoretti/credit_consultation_platform/PlataformaDeConsultaDeCreditoApplication.java
@@ -0,0 +1,13 @@
+package br.com.edsonmoretti.credit_consultation_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaDeConsultaDeCreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaDeConsultaDeCreditoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/edsonmoretti/credit_consultation_platform/PlataformaDeConsultaDeCreditoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.edsonmoretti.credit_consultation_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaDeConsultaDeCreditoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
